15f644e0ee7d5e60f111a4eadc3094f386b42c05
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-05-31 00:02:46 -0300

    Teste do codigo de funcionalidade de login.

--- src/app/enter.php
+++ src/app/enter.php
@@ -1,20 +1,23 @@
 <?php
 
 require_once __DIR__ . "/../../configurations/bootstrap.php";
+require_once __DIR__ . "/src/database/login.php";
 
 if(empty(filter_input('email')) || empty(filter_input('senha'))) {
     header('Location: http://localhost/index.php');
 }
+    
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
 if(filter_input('email')){
 
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
-
-    $mysqli = "SELECT * FROM register WHERE email = '$email' LIMIT 1";
+    return login(getConnection(),$email, $senha);
+/*     $mysqli = "SELECT * FROM register WHERE email = '$email' LIMIT 1";
     $result = $mysqli->query($mysqli) or die ($mysqli->error);
 
     $user = $result->fetch_assoc();
+    if(password_verify($senha, $user['senha'])) */
     
 
 if(login(getConnection(),$email, $senha)){
