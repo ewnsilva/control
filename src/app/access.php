65981dfbb4c8f3f9683fe48861f1a10ca6fff177
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-06-16 20:09:46 -0300

    build: Identificando as paginas de funcionalidades que o sistema terá

--- src/app/access.php
+++ src/app/access.php
@@ -5,10 +5,15 @@ require_once __DIR__ . "/../database/login.php";
 
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+if(empty($email) || empty($senha)){
+    echo '<script>alert("Prencha os campos solicitados antes de entrar.")</script>';
+    header('Location: http://localhost/index.php');
+}
    
 if(isset($email) || isset($senha)){
 
-    return login(getConnection());
+    login($conn);
     
     if($result == 1){
         $user = $sql_query->fetch_assoc();
--- src/database/login.php
+++ src/database/login.php
@@ -5,7 +5,9 @@ require_once __DIR__ . "/../../configurations/connection.php";
 function login($conn) {
     $sql_code = "SELECT * FROM register WHERE email = '$email' AND senha = '$senha'";
 
-    $sql_query = $mysqli->query($sql_code) or die ("Falha ao conectar com o banco de dados." . $mysqli->error);
+    $sql_query = $conn->query($sql_code) or die ("Falha ao conectar com o banco de dados." . $conn->error);
 
     $result = $sql_query->num_rows;
+
+    return $result;
 }
