69651cb48d55113ea0fff3059e2d43487b961644
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-06-17 23:34:27 -0300

    fix: Aplicação de correções na funcionalidade de Login

--- src/app/access.php
+++ src/app/access.php
@@ -3,23 +3,13 @@
 require_once __DIR__ . "/../../configurations/bootstrap.php";
 require_once __DIR__ . "/../database/login.php";
 
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email');
+$senha = filter_input(INPUT_POST, 'senha');
 
+$login = login(getConnection(), $email, $senha);
 
-if(isset($email) || isset($senha)){
+/* if(isset($email) || isset($senha)){
 
-    login(getConnection(), $email, $senha);
-    
-    if($result == 1){
-        $user = $statement->fetch_assoc();
-        header('/pages/home.php');
-    } else {
-        echo "Falha ao entrar no sistema.";
-    }
+    $login = login(getConnection(), $email, $senha);
 
-    if(empty($email) || empty($senha)){
-    header('Location: http://localhost/index.php');
-    }
-  
-}
+} */
--- src/database/login.php
+++ src/database/login.php
@@ -3,11 +3,15 @@
 require_once __DIR__ . "/../../configurations/connection.php";
 
 function login($conn, $email, $senha) {
-    $query = "SELECT * FROM register WHERE email = '$email' AND senha = '$senha'";
+    $query = "SELECT email FROM register WHERE email = '$email' AND senha = HASH{'$senha'}";
 
-    $statement = $conn->query($query) or die ("Falha ao conectar com o banco de dados." . $conn->error);
+    $statement = $conn->query($query);
 
     $result = $statement->num_rows;
 
-    return $result;
+    var_dump($statement);
+
+    /* if($result = 1){
+        header('/pages/home.php');
+    } */
 }
